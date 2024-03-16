a0019b98e4bfb937b3bba6ced6a7b3d10df71438
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-16 20:03:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import SignUp from './Component/Pages/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+  <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
