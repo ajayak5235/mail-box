0e4f273798b0951d78556f87bee74955edbd0848
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-21 16:30:20 +0530

    Delete src directory

--- src/Component/mail-Dash.js
+++
@@ -1,7 +0,0 @@
-const MailDash = () =>{
-    return <div style={{backgroundColor:'white', width:'100%'}} className="d-flex">
-        <h1>Welcome to email Mail Box</h1>
-    </div>
-}
-
-export default MailDash;
--- src/store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './reducer/auth-slice'
-const store =  configureStore({
-    reducer:{
-      auth: authSlice
-    }
-})
-
-export default store;
