77308ee95effca39399dd973eef0328d3cdfabdb
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-24 13:03:02 +0530

    Delete src directory

--- src/Component/EmailsInboxDetails.js
+++
@@ -1,9 +0,0 @@
-const EmailDetails = () =>{
-    return <div className='d-flex' style={{margin:'auto',width: 'auto', backgroundColor: 'white' , marginRight:'2px',marginTop:'200px'}}>
-       <div>
-        
-       </div>
-    </div>
-}
-
-export default EmailDetails;
